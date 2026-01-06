a27006b7616e2361b4b783bb31d26cf658fc18ab
firman <sahrun.nawawi@it.component.astra.co.id>
2026-01-06 21:55:13 +0530

    update app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,9 +25,7 @@ $app = new Illuminate\Foundation\Application(
 | incoming requests to this application from both the web and CLI.
 |
 */
-$app->bind('path.public', function () {
-    return __DIR__.'/../public';
-});
+
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
