9befa7acda47bf2ec8d37e3ce1e8628ca27a4e79
firman <sahrun.nawawi@it.component.astra.co.id>
2026-01-06 20:47:08 +0530

    update appserviceproviders

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,15 +4,19 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
+    public function register()
     {
-        //
+        $publicPath = base_path('public');
+        App::bind('path.public', function () use ($publicPath) {
+            return $publicPath;
+        });
     }
 
     /**
